42fddf2400d427be33ffe3cd1d80ad66f6eced64
Ellis Clayton <jirble2@gmail.com>
2014-05-25 20:49:33 +1000

    Version 1, with @ added to last waypoint. EXPERIMENTAL

---
+++ public_html/js/script.js
@@ -0,0 +1,19 @@
+function mapURL() {
+    var portalURL = document.getElementById("portals").value;
+    var portalURLs = portalURL.split(/\n/);
+    var reg = /(?:http:\/\/www.ingress.com\/intel\?ll=)(?:-)?\d{1,3}(?:.)\d{1,6}(?:,)\d{1,3}(?:.)\d{1,6}(?:&z=)\d{1,2}(?:&pll=)((?:-)?\d{1,3}(?:.)\d{1,6})(?:,)(\d{1,3}(?:.)\d{1,6})/;
+    var mapURL = "http://maps.google.com/maps/dir/";
+    if (portalURLs.length < 2) {
+        window.alert("At least two Intel URL's required.");
+    } else {
+        for (var i = 0; i < portalURLs.length; i++) {
+            var result = reg.exec(portalURLs[i]);
+            if (portalURLs.length === (i+1)) {
+                mapURL = mapURL + "@" + RegExp.$1 + "," + RegExp.$2 + "/";
+            } else {
+                mapURL = mapURL + RegExp.$1 + "," + RegExp.$2 + "/";
+            }
+        }
+    document.getElementById("map").value = mapURL;
+    }
+}
