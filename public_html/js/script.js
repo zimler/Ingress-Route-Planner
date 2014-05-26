7e78cca49d95f154fa3b05dbaee9a52f11d2aa7c
Ellis Clayton <jirble2@gmail.com>
2014-05-26 15:48:56 +1000

    Zero zoom support. Instructions added.

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,7 +1,7 @@
 function mapURL() {
     var portalURL = document.getElementById("portals").value;
     var portalURLs = portalURL.split(/\n/);
-    var reg = /(?:http(?:s{0,1}):\/\/www.ingress.com\/intel\?ll=)(?:-{0,1})?\d{1,3}(?:.)\d{1,6}(?:,)(?:-{0,1})\d{1,3}(?:.)\d{1,6}(?:&z=)\d{1,2}(?:&pll=)((?:-{0,1})?\d{1,3}(?:.{0,1})\d{1,6})(?:,)(?:-{0,1})(\d{1,3}(?:.)\d{1,6})/;
+    var reg = /(?:http(?:s{0,1}):\/\/www.ingress.com\/intel\?ll=)(?:-{0,1})?\d{1,3}(?:.)\d{1,6}(?:,)(?:-{0,1})\d{0,3}(?:(?:.)\d{1,6}){0,1}(?:&z=)\d{1,2}(?:&pll=)((?:-{0,1})?\d{1,3}(?:.{0,1})\d{1,6})(?:,)(?:-{0,1})(\d{1,3}(?:.)\d{1,6})/;
     var mapURL = "http://maps.google.com/maps/dir/";
     if (portalURLs.length < 2) {
         window.alert("At least two Intel URL's required.");
